592fe98a7a3759f45c671e428507fa617bfa81b0
Slick <ghvinerias@gmail.com>
2024-03-21 09:13:06 +0000

    hosts

--- host/main.tf
+++ host/main.tf
@@ -7,6 +7,22 @@ terraform {
     }
   }
 }
+
+provider "proxmox" {
+  pm_api_url      = var.pm_api
+  pm_user         = var.pm_user
+  pm_password     = var.pm_token
+  pm_tls_insecure = true
+  # For debugging Terraform provider errors:
+  pm_debug      = true
+  pm_log_enable = true
+  pm_log_file   = "/var/log/terraform-plugin-proxmox.log"
+  pm_log_levels = {
+    _default    = "debug"
+    _capturelog = ""
+  }
+}
+
 resource "proxmox_lxc" "dev-environment" {
   for_each        = var.lxc_hosts
   bwlimit         = 0
