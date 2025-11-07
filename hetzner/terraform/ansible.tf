3c4316a7b656f6ca9323d8e4c3a98f73469bf413
Aleksandre Ghvineria <ghvinerias@gmail.com>
2025-11-07 22:14:46 +0400

    terraform was not triggering ansible when adding/removing node from cluster, now it's fixed.

--- hetzner/terraform/ansible.tf
+++ hetzner/terraform/ansible.tf
@@ -5,8 +5,16 @@
 resource "null_resource" "install_and_provision_K8S_Cluster" {
 
   depends_on = [
-    hcloud_server.k3s_nodes,
+    hcloud_server.k8s_nodes,
   ]
+
+  # Re-run when node configuration changes (e.g., node added/removed/modified)
+  # Using a hash of var.node_configs ensures this resource is replaced when the
+  # cluster topology changes, triggering the local-exec provisioner again.
+  triggers = {
+    nodes_hash  = sha1(jsonencode(var.node_configs))
+    nodes_count = tostring(length(var.node_configs))
+  }
 
   provisioner "local-exec" {
     working_dir = "/Users/aleksandre_ghvineria/Code/homelab/hetzner/Ansible"
